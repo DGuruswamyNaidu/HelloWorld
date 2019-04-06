02a38b1641b852e892f7fc4b6bc89167e6de1727
Guruswamy <49345997+DGuruswamyNaidu@users.noreply.github.com>
2019-04-06 22:00:15 +0530

    Create Hello.cs

---
+++ Hello.cs
@@ -0,0 +1,12 @@
+using System;
+namespace HelloWorld
+{
+  public class myClass
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hello World");
+      Console.ReadLine();
+    }
+  }
+}
